7b712760add412885536248e3f40820e9ce77f75
Jeremiah Reid <jeremiahdelacruzreid@gmail.com>
2023-01-12 18:29:26 -0500

    Halfway working form

---
+++ my-app/src/components/Home.jsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import  Button  from 'react-bootstrap/Button';
+import  Form  from 'react-bootstrap/Form';
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Home = () => {
+  const [name, SetName] = useState("");
+  const [password, SetPassword] = useState("");
+  const Testing  = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}, and your password is: ${password}`);
+  }
+    return(
+// Form to signup
+      <>
+      <h1>Sign up here!</h1>
+      <form onSubmit={Testing}>
+        <fieldset>
+          <label>Set your username here! 
+            <input 
+              type="name" 
+              value={name} 
+              onChange={(e) => SetName(e.target.value)} 
+            />
+          </label>
+          <label>Set your Password here!
+            <input
+              type="password"
+              value={password}
+              onchange={(e) => SetPassword(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </fieldset>
+      </form>
+      </>
+    );
+};
+
+export default Home;
+
