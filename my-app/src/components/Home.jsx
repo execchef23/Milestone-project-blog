a899d155eae9ca02f74da3637f758f404ec58ef4
Amanda Sharmaine Remo <asremo@Amandas-MBP.lan>
2023-01-15 11:43:01 -0500

    changed home page code

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,21 +1,10 @@
 import NavBar from './NavBar';
 import ViewBlogs from './ViewBlogs';
-// import kitty from "../assets/images/kitty.jpeg"
-import { useState } from 'react';
-// import ReactDOM from 'react-dom/client';
 
 const Home = () => {
     return(
       <>
         <NavBar />
-        {/* <div class="card">
-          <a href="">
-            <div class="container">
-              <h4><b>John Doe</b></h4>
-              <p>Architect & Engineer</p>
-            </div>
-          </a>
-        </div> */}
         <div className='blogs-container'>
           <ViewBlogs />
         </div>
