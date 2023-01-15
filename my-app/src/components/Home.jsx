806f942e4960226af9ffaf29a6bea502bf491135
Jeremiah Reid <jeremiahdelacruzreid@gmail.com>
2023-01-15 11:22:06 -0500

    fixing...

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -5,42 +5,12 @@ import { Link } from "react-router-dom";
 // import ReactDOM from 'react-dom/client';
 
 const Home = () => {
-  const [state, setState] = useState([]);
 
-  const getData = async () => {
-    // console.log("We got clicked");
-    const data = await fetch("http://localhost:3001/blogs");
-    console.log("DATA inital from backed", data);
-    const cleanData = await data.json();
-    console.log("STUFF FROM BACKNED!!", cleanData);
-    setState(cleanData);
-  };
-  console.log("State", state);
-
-  useEffect(() => {
-    getData();
-  }, []);
     return(
       <>
   <NavBar />
-  {state.map((blogs) => {
-    return(
-      <div class="card">
-  <a className="link" href={`/blogs/${blogs._id}`} > {blogs.title}
-  <div key={blogs._id} className="col-sm-6"> </div>
-  <img
-         type="url"
-          src={blogs.image}
-          className="placeimg"
-          alt={blogs.title}
-        />
-    <p> Description: {blogs.description}</p>
-  </a>
-</div>
- 
-  );
-  })}
-      {/* <ViewBlogs /> */}
+
+      <ViewBlogs />
       </>
     );
 };
