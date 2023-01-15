689480c7f77fbaabd3037e92f61b0f765c3e73d8
Jeremiah Reid <jeremiahdelacruzreid@gmail.com>
2023-01-15 10:20:35 -0500

    Cards are now working

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,39 +1,24 @@
 import NavBar from './NavBar';
 import ViewBlogs from './ViewBlogs';
+import kitty from "../assets/images/kitty.jpg"
 import { useState } from 'react';
 // import ReactDOM from 'react-dom/client';
 
 const Home = () => {
-  const [name, SetName] = useState("");
-  const [password, SetPassword] = useState("");
-  const Testing  = (event) => {
-    event.preventDefault();
-    alert(`The name you entered was: ${name}, and your password is: ${password}`);
-  }
     return(
-// Form to signup
       <>
   <NavBar />
-      <h1>Sign up here!</h1>
-      <form onSubmit={Testing}>
-        <fieldset>
-          <label>Set your username here! 
-            <input 
-              type="name" 
-              value={name} 
-              onChange={(e) => SetName(e.target.value)} 
-            />
-          </label>
-          <label>Set your Password here!
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => SetPassword(e.target.value)}
-            />
-          </label>
-          <input type="submit" />
-        </fieldset>
-      </form>
+  <div class="card">
+    <a href="">
+  <img src={kitty}
+       alt="Blog"  
+       />
+  <div class="container">
+    <h4><b>John Doe</b></h4>
+    <p>Architect & Engineer</p>
+  </div>
+  </a>
+  </div>
       <ViewBlogs />
       </>
     );
