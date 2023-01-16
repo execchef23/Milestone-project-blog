e78aba5c4324624be68e112c258408fa008813fe
AriesRedWar <philipjbowman5@gmail.com>
2023-01-15 20:06:57 -0500

    took navbar from multiple jsx forms and added to the Main app.js to lower the amount of times its imported repeated code. added CSS for navbar and footer. fixed Editblog button to go to home after clicking button.

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,4 +1,3 @@
-import NavBar from './NavBar';
 import ViewBlogs from './ViewBlogs';
 // import { useState } from 'react';
 import "react-bootstrap/Container";
@@ -7,7 +6,6 @@ import "react-bootstrap/Container";
 const Home = () => {
     return(
       <>
-        <NavBar />
         <ViewBlogs />
       </>
     );
