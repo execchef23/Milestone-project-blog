88413a4abbbdd85307b39607d28a020cff9d29ec
AriesRedWar <philipjbowman5@gmail.com>
2023-01-19 22:12:24 -0500

    fixed package and issues in server.js

--- my-app/src/components/LogIn.jsx
+++ my-app/src/components/LogIn.jsx
@@ -1,70 +1,68 @@
-import { useState, useContext } from 'react'
-import { CurrentUserContext } from '../contexts/CurrentUser';
-import { useNavigate } from 'react-router-dom';
+import { useState, useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUser";
+import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [credentials, setCredentials] = useState({
-        username: "",
-        password: ""
-    })
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
 
-    const {setCurrentUser} = useContext(CurrentUserContext)
+  const { setCurrentUser } = useContext(CurrentUserContext);
 
-    async function handleSubmit (e){
-        e.preventDefault()
-        const response = await fetch("http://localhost:3001/auth", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(credentials)
-        })
-        const data = await response.json()
-        if (response.status===200){
-            const {user, token} = data
-            localStorage.setItem("token", token)
-            setCurrentUser(user)
-            navigate('/')
-        }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3001/auth", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    const data = await response.json();
+    if (response.status === 200) {
+      const { user, token } = data;
+      localStorage.setItem("token", token);
+      setCurrentUser(user);
+      navigate("/");
     }
+  }
 
-    return(
-    
-        <div>
-            <div className="Form">
-                <h2>Log In</h2>
-                <form action="/login" method="get">
-
-                    <label>Username: </label>
-                    <input 
-                        id="username"
-                        name="username"
-                        required
-                        onChange={e => setCredentials({
-                            ...credentials,
-                            username: e.target.value
-                        })}
-                    />
-                    <label>Password: </label>
-                    <input 
-                        id="password"
-                        name="password"
-                        required
-                        type="password"
-                        onChange={e => setCredentials({
-                            ...credentials,
-                            password: e.target.value
-                        })}
-                    />
-                    <input 
-                        onClick={LogIn}
-                        className="btn"
-                        value="Log In"
-                    />
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="Form">
+        <h2>Log In</h2>
+        <form action="/login" method="get">
+          <label>Username: </label>
+          <input
+            id="username"
+            name="username"
+            required
+            onChange={(e) =>
+              setCredentials({
+                ...credentials,
+                username: e.target.value,
+              })
+            }
+          />
+          <label>Password: </label>
+          <input
+            id="password"
+            name="password"
+            required
+            type="password"
+            onChange={(e) =>
+              setCredentials({
+                ...credentials,
+                password: e.target.value,
+              })
+            }
+          />
+          <input onClick={LogIn} className="btn" value="Log In" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
