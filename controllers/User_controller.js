35f8b7ee9553647769106d420ed5b329bd2ee280
AriesRedWar <philipjbowman5@gmail.com>
2023-01-22 13:34:17 -0500

    vercel

--- controllers/User_controller.js
+++ controllers/User_controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.get("/", (req, res) => {
   console.log("WE SMACKED THE GET ROUTER /Users !!!");
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 // const bcrypt   = require('bcrypt-nodejs');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 
 
