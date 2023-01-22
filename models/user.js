36cefa77797fec357da8cc4e9eff2870ea482226
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2023-01-22 10:30:22 -0500

    updated auth_controller js and user.js context file

--- models/user.js
+++ models/user.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt   = require('bcrypt-nodejs');
 
+
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  password: { data: Buffer, type: String },
-  email: { type: String },
+  password: { data: Buffer, type: String, required: true },
+  // passwordDigest: {type: String}
+  // email: { type: String },
 });
 
 userSchema.methods.generateHash = function(password) {
@@ -15,6 +18,8 @@ userSchema.methods.generateHash = function(password) {
 userSchema.methods.verifyPassword = function(password) {
   return bcrypt.compareSync(password, this.user.password);
 };
+
+// 
 
 userSchema.methods.updateUser = function(request, response){
 	this.user.name = request.body.name;
