9fb3ec68e0f10279c6302013d369f518e195075e
AriesRedWar <philipjbowman5@gmail.com>
2023-01-28 10:46:40 -0500

    commeted out some console logs

--- my-app/src/components/NewBlog.jsx
+++ my-app/src/components/NewBlog.jsx
@@ -1,73 +1,77 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const NewBlog = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [title, setTitle] = useState("");
+  // console.log('title!!!', title)
 
-    const [title, setTitle] = useState('')
-    // console.log('title!!!', title)
+  const [image, setImage] = useState("");
+  // console.log('image!!!', image)
 
-    const [image, setImage] = useState('')
-    // console.log('image!!!', image)
+  const [description, setDescription] = useState("");
+  // console.log('description!!!', description)
 
-    const [description, setDescription] = useState('')
-    // console.log('description!!!', description)
+  const submitBlog = async () => {
+    console.log("We are about to save!!!!!");
 
-    const submitBlog = async () => {
-        console.log('We are about to save!!!!!')
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              title: title,
-              image: image,
-              description: description
-            })
-          };
+    navigate("/")
 
-        const data = await fetch("/blogs", requestOptions)
-        console.log('time to save blog!!', data)
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: title,
+        image: image,
+        description: description,
+      }),
+    };
 
-        navigate('/')
-    }
+    const data = await fetch("/blogs", requestOptions);
+    console.log("time to save blog!!", data)
+  };
 
-    return (
-
-        <div>
-            <div className="Form">
-            <h2>Add A New Blog: </h2>
-            <form >
-                <label>Title: </label>
-                <input 
-                    id="title"
-                    name="title"
-                    required
-                    onChange={(e)=> {setTitle(e.target.value)}}
-                />
-                <label>Image URL (optional): </label>
-                <input 
-                    type="url"
-                    id="image"
-                    name="image"
-                    onChange={(e)=> {setImage(e.target.value)}}
-                />
-                <label>Description: </label>
-                <textarea
-                    id="description"
-                    name="description" 
-                    required
-                    onChange={(e)=> {setDescription(e.target.value)}}
-                />
-                <input 
-                    onClick={submitBlog}
-                    className="btn"
-                    value="Submit Blog"
-                />
-            </form>
-        </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="Form">
+        <h2>Add A New Blog: </h2>
+        <form>
+          <label>Title: </label>
+          <input
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
+          />
+          <label>Image URL (optional): </label>
+          <input
+            type="url"
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+          <label>Description: </label>
+          <textarea
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            value={description}
+          />
+                  <Button
+          onClick={submitBlog}
+          type="submit"
+          disabled={!title || !description }
+          variant="secondary"
+          size="lg"
+        >
+          Submit
+        </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default NewBlog;
